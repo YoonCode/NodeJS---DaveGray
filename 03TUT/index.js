21ce731381f6539154bafe89fb346b7d6458a27d
YoonCode <yjin830201@gmail.com>
2022-06-14 17:09:10 +0900

    install dependencies(date-fns, uuid), devDependencies(nodemon)

--- 03TUT/index.js
+++ 03TUT/index.js
@@ -1,5 +1,9 @@
 const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
+// const uuid = require('uuid')
 
 console.log(format(new Date(), 'yyyyMMdd\tHH:MM:ss'))
 
-console.log('hello')
+console.log(uuid()) // uuid : generate random id
+
+console.log()
